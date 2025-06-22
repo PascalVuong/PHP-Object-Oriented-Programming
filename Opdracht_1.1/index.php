2df903d261a92510b440eef8091d4b618a15d4c1
Pascal_Vuong <9008819@student.zadkine.nl>
2025-06-22 18:31:47 +0200

    Initial commit

---
+++ Opdracht_1.1/index.php
@@ -0,0 +1,11 @@
+<?php
+$firstName = "Pascal";
+$age = 29;
+$height = 1.75;
+$isStudent = True;
+
+echo "Mijn naam is: {$firstName}.<br>";
+echo "Mijn leeftijd is: {$age}.<br>";       
+echo "Mijn lengte is {$height}m.<br>";
+echo "Ben ik student? {$isStudent}. (1 staat voor True, 0 staat voor False.)";
+?>
