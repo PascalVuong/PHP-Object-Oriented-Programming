3fa370f2863f55551e92e32d05127c925ed433e2
Pascal_Vuong <9008819@student.zadkine.nl>
2025-06-23 11:19:14 +0200

    opdracht 1.3 voldaan

---
+++ Opdracht_1.3/index.php
@@ -0,0 +1,10 @@
+<?php
+$city = "Amsterdam";
+$population = 821752;
+$averageTemperature = 17.5;
+$isCapital = True;
+echo "Ik woon in {$city}.<br>";
+echo "De populatie is: {$population}.<br>";
+echo "Het gemiddelde temperatuur is {$averageTemperature}.<br>";
+echo "De boolean van de hoofdstad is: {$isCapital}, dus True.<br>";
+?>
